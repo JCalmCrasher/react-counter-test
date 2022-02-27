0b27bdf5df71cfd17b0aba9b1385722688aecfda
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-02-27 05:12:02 +0100

    :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setNumber(number - 1)}>Decrease</button>
+      <p data-testid="counter">{number}</p>
+      <button onClick={() => setNumber(number + 1)}>Increase</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Counter", () => {
+  test("increase when the increment button in clicked", () => {
+    render(<App />);
+
+    const button = screen.getByText(/increase/i);
+    fireEvent.click(button);
+
+    const paragraph = screen.getByTestId("counter");
+    expect(paragraph).toHaveTextContent(/1/i);
+  });
+
+  test("decrease when the decrement button in clicked", () => {
+    render(<App />);
+
+    const button = screen.getByText(/decrease/i);
+    const paragraph = screen.getByTestId("counter");
+
+    fireEvent.click(button);
+    expect(paragraph.textContent).toBe("-1");
+  });
 });
