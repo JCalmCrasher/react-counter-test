f88df74988a9e24db14715e68bdeac8987b71ebe
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-03-01 21:14:05 +0100

    :fire:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [number, ] = useState(0);
 
   return (
     <div className="App">
-      <button>Decrease</button>
-      <p>{number}</p>
-      <button>Increase</button>
     </div>
   );
 }
