a4ed4d496b148bbbe8fc7b44cee74bc1e4de6a35
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-03-01 20:21:41 +0100

    :init: `App.js` starter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState(0);
+  const [number, ] = useState(0);
+
   return (
     <div className="App">
-      <button onClick={() => setNumber(number - 1)}>Decrease</button>
+      <button>Decrease</button>
       <p data-testid="counter">{number}</p>
-      <button onClick={() => setNumber(number + 1)}>Increase</button>
+      <button>Increase</button>
     </div>
   );
 }
