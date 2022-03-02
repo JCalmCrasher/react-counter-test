58c481ac4a2880244d61dfac6b9cd2529637901b
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-03-02 16:45:17 +0100

    :fire:

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,8 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("Counter", () => {
-  it("should increase when the increment button in clicked", () => {
-    render(<App />);
-
-    const button = screen.getByText(/increase/i);
-    fireEvent.click(button);
-
-    const paragraph = screen.getByTestId("counter");
-    expect(paragraph).toHaveTextContent(/1/i);
-  });
-
-  it("should decrease when the decrement button in clicked", () => {
-    render(<App />);
-
-    const button = screen.getByText(/decrease/i);
-    const paragraph = screen.getByTestId("counter");
-
-    fireEvent.click(button);
-    expect(paragraph.textContent).toBe("-1");
-  });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
